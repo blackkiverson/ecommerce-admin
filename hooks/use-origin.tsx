d505947f6e8ebf591359f1e60ce1da499cdc2b58
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2023-08-11 13:15:28 +0100

    ...

--- hooks/use-origin.tsx
+++ hooks/use-origin.tsx
@@ -1,5 +1,11 @@
 import { useEffect, useState } from "react";
 
+/**
+ * The `useOrigin` function returns the origin of the current window location in a React component, or
+ * an empty string if it is not available.
+ * @returns The function `useOrigin` returns the origin of the current window location as a string. If
+ * the window object is not available or the origin is not defined, an empty string is returned.
+ */
 export const useOrigin = () => {
     const [mounted, setMounted] = useState(false);
     const origin = typeof window !== "undefined" && window.location.origin ? window.location.origin : '';
